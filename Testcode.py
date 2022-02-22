34d96e1c5d3861622af475c6aa82d17ef11ccee8
Simon Kaserer <simon.kaserer@gmail.com>
2022-02-22 15:04:50 +0100

    Warping of the outer square edge implemented

--- Testcode.py
+++ Testcode.py
@@ -9,119 +9,73 @@ import os
 
 #open a picture from the desktop:
 path= '/home/pi/Desktop/'
-picture = cv2.imread('/home/pi/Desktop/testLeftkey.jpg')
-
-# vlt. einen geradheitsabgleich machen ?! zum entzerren des randes
-
-# und evtl canny alg. laufen lassen
-
-
+picture = cv2.imread('/home/pi/Desktop/testRgb.jpg')
 
 
 gray=cv2.cvtColor(picture,cv2.COLOR_BGR2GRAY)
-ret,thresh=cv2.threshold(gray,200,255,0)
-#cv2.imshow("thresh",thresh)
-#cv2.waitKey(0)
-
-# no blur!
-#blur = cv2.medianBlur(gray,3)
-#cv2.imshow("blur",blur)
-#cv2.waitKey(0)
-blur=thresh
-
-# maybe helpful
-#edges=cv2.Canny(blur,100,200)
-#print(len(edges))
-#cv2.imshow("canny",edges)
-#cv2.waitKey(0)
-#cv2.imwrite(os.path.join(path,'Canny.jpg'),edges)
+ret,thresh=cv2.threshold(gray,160,255,0)
  
-contours,hierarchy = cv2.findContours(blur,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-#print("Contours 1: ")
-#print(len(contours))
-pic=cv2.cvtColor(blur,cv2.COLOR_GRAY2BGR)
+contours,hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
 
-# Visualize the first contour
-#cont1=cv2.drawContours(pic,contours,0,(0,255,0),1)
-#cv2.imshow('cont1',cont1)
-#cv2.waitKey(0)
-
-
-#find max area contour
+# Find the max-area contour of the outer line:
 if len(contours) > 0:
     cnt = sorted(contours,key=cv2.contourArea)[-1]
-    (x,y),(w,h),a=cv2.minAreaRect(cnt)
-    print("Turning angle:")
-    print(a)
-    rot_mat=cv2.getRotationMatrix2D((x,y),a,1)
-    rotated_image=cv2.warpAffine(blur,rot_mat,(int(w+x),int(h+y)))
-    #points=cv2.boxPoints(cnt)
+    # warp the contour into a straight rectangle:
+        # find the cornerpoints of the square
+    epsilon=0.01*cv2.arcLength(cnt,True)
+        # use the Douglas-Peucker algorithm for approximating a rectangle shape
+    outer_square=cv2.approxPolyDP(cnt,epsilon,True)
+        # save the points in seperate variables
+    pt_A=outer_square[0]
+    pt_B=outer_square[1]
+    pt_C=outer_square[2]
+    pt_D=outer_square[3]
+        #calculate the lengt and the width of the square
+    width1=int(np.sqrt(((pt_A[0][0]-pt_D[0][0])**2)+(pt_A[0][1]-pt_D[0][1])**2))
+    height1=int(np.sqrt(((pt_A[0][0]-pt_B[0][0])**2)+(pt_A[0][1]-pt_B[0][1])**2))
+    width2=int(np.sqrt(((pt_B[0][0]-pt_C[0][0])**2)+(pt_B[0][1]-pt_C[0][1])**2))
+    height2=int(np.sqrt(((pt_C[0][0]-pt_D[0][0])**2)+(pt_C[0][1]-pt_D[0][1])**2))
+    width=max(width1,width2)
+    height=max(height1,height2)
+    #print("width, height")
+    #print(width,height)
+    input_pts=np.float32([pt_A,pt_B,pt_C,pt_D])
+    output_pts=np.float32([[0,0],[0,height],[width,height],[width,0]])
+    transf_matrix=cv2.getPerspectiveTransform(input_pts,output_pts,)
+    warped_image=cv2.warpPerspective(thresh,transf_matrix,(width,height),flags=cv2.INTER_LINEAR)
+        # crop the image to remove the outer edge (offset can maybe be smaller when camera calibration is done?)
+    offset=5
+    warped_image=warped_image[0+offset:width-offset,0+offset:height-offset]
+    # #resize the image to display properly
+    # cv2.imshow("warpedtest",cv2.resize(warped_image,(600,600)))
+    # key=cv2.waitKey(0)
+    # if key==ord('q'):
+    #     quit()
+    
 
-    #cv2.imshow("rotated",rotated_image)
-    #cv2.waitKey(0)
 
-    # Find the max-area contour of the outer line:
-    cnts,hierarchy=cv2.findContours(rotated_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    #print("contours rotated")
-    #print(len(cnts))
+
+    cnts,hierarchy=cv2.findContours(warped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    #cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_TC89_KCOS)
+
+ 
     if len(cnts)>0:
         cnt=sorted(cnts,key=cv2.contourArea)[-1]
-        epsilon=0.1*cv2.arcLength(cnt,True)
-        # use the Douglas-Peucker algorithm for approximating a rectangle shape
+        epsilon=0.0005*cv2.arcLength(cnt,True)
+        #   use the Douglas-Peucker algorithm for approximating a rectangle shape
         approx=cv2.approxPolyDP(cnt,epsilon,True)
-        #find a straight bounding rectangle inside the approximated one
-        x,y,w,h=cv2.boundingRect(approx)
-        #print("Bounding rect:")
-        #print(x)
-        #print(y)
-        #print(w)
-        #print(h)
-        # rotated_image=rotated_image[y:y+h,x:x+w].copy()
-        # some pixel get cut away to reach the inner side of the contour
-        offset=12
-        # this offset can be smaller if the outer contour is straight and only 1 px 
-        print("width")
-        print(w-(2*offset))
-        print("height")
-        print(h-(2*offset))
-        cropped_image=rotated_image[y+offset:y+h-offset,x+offset:x+w-offset]
-        #cv2.imshow("cropped",cropped_image)
-        #cv2.waitKey(0)
-
+        print("Number of appr. points:")
+        print(len(approx))
         
 
-        cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-        #cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_TC89_KCOS)
-        #for testing:
-        pic=cv2.cvtColor(cropped_image,cv2.COLOR_GRAY2BGR)
-        #testcont=cv2.drawContours(pic,cnts,-1,(0,255,0),3)
-        #print("Number of contour points:")
-        #print(len(cnts))
-        #cv2.imshow("testcontour",testcont)
-        #cv2.waitKey(0)
-        if len(cnts)>0:
-            cnt=sorted(cnts,key=cv2.contourArea)[-1]
-            epsilon=0.0005*cv2.arcLength(cnt,True)
-            #   use the Douglas-Peucker algorithm for approximating a rectangle shape
-            approx=cv2.approxPolyDP(cnt,epsilon,True)
-            print("Number of appr. points:")
-            print(len(approx))
-            
+        inv=cv2.cvtColor(warped_image,cv2.COLOR_GRAY2BGR)
+        cont_tool=cv2.drawContours(inv,[approx],-1,(0,255,0),3)
 
-            inv=cv2.cvtColor(cropped_image,cv2.COLOR_GRAY2BGR)
-            cont_tool=cv2.drawContours(inv,[approx],-1,(0,255,0),3)
-
-            cv2.imshow("tool-curve",cont_tool)
-            cv2.waitKey(0)
+        cv2.imshow("tool-curve",cv2.resize(cont_tool,(600,600)))
+        cv2.waitKey(0)
 
 
 
 
 
 
-#print("Contours 2: ")
-#print(len(contours))
-
-#cont2=cv2.drawContours(pic,contours,1,(0,255,0),1)
-#cv2.imwrite(os.path.join(path,'Contour1.jpg'),cont1)
-#cv2.imwrite(os.path.join(path,'Contour2.jpg'),cont2)
