510e9ccab3cc3b10f399fa36aaf46884d46acc6c
Simon Kaserer <simon.kaserer@gmail.com>
2022-02-22 16:35:01 +0100

    Put the extration part into the Function.py file

--- Contour_Extraction.py
+++ Contour_Extraction.py
@@ -7,6 +7,7 @@ import depthai as dai
 import numpy as np
 # added
 import os
+import Functions as fct
 #from threading import Timer
 
 def saveimg():
@@ -15,76 +16,6 @@ def saveimg():
     cv2.imwrite(os.path.join(path,'testRight.jpg'),edgeRightFrame)
     cv2.imwrite(os.path.join(path,'testRgb.jpg'),edgeRgbFrame)
 
-
-def extraction(img,printsize,printpoints):
-    ret,thresh=cv2.threshold(img,thresh_val,255,0)
-    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
-
-    # Find the max-area contour of the outer line:
-    if len(contours) > 0:
-        cnt = sorted(contours,key=cv2.contourArea)[-1]
-        #Test: Show the found contour:
-        pic=cv2.cvtColor(thresh,cv2.COLOR_GRAY2BGR)
-        edge_out=cv2.drawContours(pic,[cnt],-1,(255,0,0),2)
-        cv2.imshow("outer edge",cv2.resize(edge_out,(720,500)))
-        # warp the contour into a straight rectangle:
-            # find the cornerpoints of the square
-        epsilon=0.01*cv2.arcLength(cnt,True)
-            # use the Douglas-Peucker algorithm for approximating a rectangle shape
-        outer_square=cv2.approxPolyDP(cnt,epsilon,True)
-            # save the points in seperate variables
-        if len(outer_square) ==4:
-            pt_A=outer_square[0]
-            pt_B=outer_square[1]
-            pt_C=outer_square[2]
-            pt_D=outer_square[3]
-            #calculate the lengt and the width of the square
-            width1=int(np.sqrt(((pt_A[0][0]-pt_D[0][0])**2)+(pt_A[0][1]-pt_D[0][1])**2))
-            height1=int(np.sqrt(((pt_A[0][0]-pt_B[0][0])**2)+(pt_A[0][1]-pt_B[0][1])**2))
-            width2=int(np.sqrt(((pt_B[0][0]-pt_C[0][0])**2)+(pt_B[0][1]-pt_C[0][1])**2))
-            height2=int(np.sqrt(((pt_C[0][0]-pt_D[0][0])**2)+(pt_C[0][1]-pt_D[0][1])**2))
-            width=max(width1,width2)
-            height=max(height1,height2)
-            if printsize:
-                print("width, height")
-                print(width,height)
-            input_pts=np.float32([pt_A,pt_B,pt_C,pt_D])
-            output_pts=np.float32([[0,0],[0,height],[width,height],[width,0]])
-            transf_matrix=cv2.getPerspectiveTransform(input_pts,output_pts,)
-            warped_image=cv2.warpPerspective(thresh,transf_matrix,(width,height),flags=cv2.INTER_LINEAR)
-                # crop the image to remove the outer edge (offset can maybe be smaller when camera calibration is done?)
-            offset=2
-            warped_image=warped_image[0+offset:width-offset,0+offset:height-offset]
-
-        # #resize the image to display properly
-        # cv2.imshow("warpedtest",cv2.resize(warped_image,(600,600)))
-        # key=cv2.waitKey(0)
-        # if key==ord('q'):
-        #     quit()
-        
-
-
-
-            cnts,hierarchy=cv2.findContours(warped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-            #cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_TC89_KCOS)
-
-        
-            if len(cnts)>0:
-                cnt=sorted(cnts,key=cv2.contourArea)[-1]
-                epsilon=factor*cv2.arcLength(cnt,True)
-                #   use the Douglas-Peucker algorithm for approximating a rectangle shape
-                approx=cv2.approxPolyDP(cnt,epsilon,True)
-                if printpoints:
-                    print("Number of appr. points:")
-                    print(len(approx))
-                
-
-                inv=cv2.cvtColor(warped_image,cv2.COLOR_GRAY2BGR)
-                cont_tool=cv2.drawContours(inv,[approx],-1,(0,255,0),3)
-
-                cv2.imshow("tool-curve",cv2.resize(cont_tool,(600,600)))
-        else:
-            print("Outer square not found!")
 
 
 # Create pipeline
@@ -183,13 +114,13 @@ with dai.Device(pipeline) as device:
         #cv2.imshow(edgeRgbStr, cv2.resize(edgeRgbFrame,(720,500)))
        
         # add the contour extraction here:
-        extraction(edgeRgbFrame,False,False)
+        fct.extraction_polyDP(edgeRgbFrame,factor,thresh_val,False,False)
         
             
             
         
 
-        key = cv2.waitKey(500)
+        key = cv2.waitKey(1000)
         if key == ord('q'):
             break
 
