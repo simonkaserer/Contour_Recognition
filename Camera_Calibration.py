762fbfca1f8aed206d3e85da1935da40b7a27f9a
Simon Kaserer <simon.kaserer@gmail.com>
2022-02-22 10:01:56 +0100

    Added the pipeline creation to the file - the parameter have to be adopted to get simply the frames to calibrate the three cameras

--- Camera_Calibration.py
+++ Camera_Calibration.py
@@ -1,1 +1,43 @@
 # This code calibrates the three cameras with OpenCV and a pattern and saves the camera parameters into configuration files
+
+#!/usr/bin/env python3
+
+import cv2
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+camRgb = pipeline.create(dai.node.ColorCamera)
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+
+cameraLeft = pipeline.create(dai.node.???)
+cameraRight = pipeline.create(dai.node.???)
+cameraRgb = pipeline.create(dai.node.???)
+
+xoutEdgeLeft = pipeline.create(dai.node.XLinkOut)
+xoutEdgeRight = pipeline.create(dai.node.XLinkOut)
+xoutEdgeRgb = pipeline.create(dai.node.XLinkOut)
+xinEdgeCfg = pipeline.create(dai.node.XLinkIn)
+
+edgeLeftStr = "edge left"
+edgeRightStr = "edge right"
+edgeRgbStr = "edge rgb"
+edgeCfgStr = "edge cfg"
+
+xoutEdgeLeft.setStreamName(edgeLeftStr)
+xoutEdgeRight.setStreamName(edgeRightStr)
+xoutEdgeRgb.setStreamName(edgeRgbStr)
+xinEdgeCfg.setStreamName(edgeCfgStr)
+
+# Properties
+camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_800_P)
+#monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_800_P)
+#monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
