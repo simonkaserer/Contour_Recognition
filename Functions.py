d6a65350725f3238449de21c36e1b43e59ccaab2
Simon Kaserer <simon.kaserer@gmail.com>
2022-02-22 16:57:10 +0100

    Added a convex hull extraction function

--- Functions.py
+++ Functions.py
@@ -43,10 +43,9 @@ def extraction_polyDP(img,factor_epsilon,threshold_value,printsize,printpoints):
             offset=2
             warped_image=warped_image[0+offset:width-offset,0+offset:height-offset]
 
-
+            # Look for the contour of the tool:
             cnts,hierarchy=cv2.findContours(warped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
             #cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_TC89_KCOS)
-
         
             if len(cnts)>0:
                 cnt=sorted(cnts,key=cv2.contourArea)[-1]
@@ -60,6 +59,60 @@ def extraction_polyDP(img,factor_epsilon,threshold_value,printsize,printpoints):
 
                 inv=cv2.cvtColor(warped_image,cv2.COLOR_GRAY2BGR)
                 cont_tool=cv2.drawContours(inv,[approx],-1,(0,255,0),3)
+
+                cv2.imshow("tool-curve",cv2.resize(cont_tool,(600,600)))
+        else:
+            print("Outer square not found!")
+
+def extraction_convexHull(img,threshold_value):
+    ret,thresh=cv2.threshold(img,threshold_value,255,0)
+    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+
+    # Find the max-area contour of the outer line:
+    if len(contours) > 0:
+        cnt = sorted(contours,key=cv2.contourArea)[-1]
+        #Test: Show the found contour:
+        pic=cv2.cvtColor(thresh,cv2.COLOR_GRAY2BGR)
+        edge_out=cv2.drawContours(pic,[cnt],-1,(255,0,0),2)
+        cv2.imshow("outer edge",cv2.resize(edge_out,(720,500)))
+        # warp the contour into a straight rectangle:
+            # find the cornerpoints of the square
+        epsilon=0.01*cv2.arcLength(cnt,True)
+            # use the Douglas-Peucker algorithm for approximating a rectangle shape
+        outer_square=cv2.approxPolyDP(cnt,epsilon,True)
+            # save the points in seperate variables
+        if len(outer_square) ==4:
+            pt_A=outer_square[0]
+            pt_B=outer_square[1]
+            pt_C=outer_square[2]
+            pt_D=outer_square[3]
+            #calculate the lengt and the width of the square
+            width1=int(np.sqrt(((pt_A[0][0]-pt_D[0][0])**2)+(pt_A[0][1]-pt_D[0][1])**2))
+            height1=int(np.sqrt(((pt_A[0][0]-pt_B[0][0])**2)+(pt_A[0][1]-pt_B[0][1])**2))
+            width2=int(np.sqrt(((pt_B[0][0]-pt_C[0][0])**2)+(pt_B[0][1]-pt_C[0][1])**2))
+            height2=int(np.sqrt(((pt_C[0][0]-pt_D[0][0])**2)+(pt_C[0][1]-pt_D[0][1])**2))
+            width=max(width1,width2)
+            height=max(height1,height2)
+
+            input_pts=np.float32([pt_A,pt_B,pt_C,pt_D])
+            output_pts=np.float32([[0,0],[0,height],[width,height],[width,0]])
+            transf_matrix=cv2.getPerspectiveTransform(input_pts,output_pts,)
+            warped_image=cv2.warpPerspective(thresh,transf_matrix,(width,height),flags=cv2.INTER_LINEAR)
+                # crop the image to remove the outer edge (offset can maybe be smaller when camera calibration is done?)
+            offset=2
+            warped_image=warped_image[0+offset:width-offset,0+offset:height-offset]
+
+            # Look for the contour of the tool:
+            cnts,hierarchy=cv2.findContours(warped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+        
+            if len(cnts)>0:
+                cnt=sorted(cnts,key=cv2.contourArea)[-1]
+                
+                hull=cv2.convexHull(cnt)
+                
+                inv=cv2.cvtColor(warped_image,cv2.COLOR_GRAY2BGR)
+                cont_tool=cv2.drawContours(inv,[hull],-1,(0,255,0),3)
 
                 cv2.imshow("tool-curve",cv2.resize(cont_tool,(600,600)))
         else:
