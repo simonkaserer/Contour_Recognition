6141326fe65ab52bb31317a9027c39092e9de153
Simon Kaserer <simon.kaserer@gmail.com>
2022-03-02 18:34:39 +0100

    Functioning calibration algorithmus

--- Testcode.py
+++ Testcode.py
@@ -2,12 +2,14 @@
 
 #!/usr/bin/env python3
 
+from traceback import FrameSummary
 import cv2
 import numpy as np
 import glob
 import yaml
 
-images_left = glob.glob('MCI_Contour_Recognition/CalPicsLeft/*.jpg')
+images_left = glob.glob('./CalPicsLeft/*.jpg')
+print(f"{len(images_left)} pictures added")
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -17,9 +19,12 @@ objp_left[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
 objpoints_left = [] # 3d point in real world space
 imgpoints_left = [] 
 
+
+
 for fname in images_left:
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
         # Find the chess board corners
         ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
         # If found, add object points, image points (after refining them)
@@ -29,13 +34,25 @@ for fname in images_left:
             imgpoints_left.append(corners)
             # Draw and display the corners
             cv2.drawChessboardCorners(img, (8,6), corners2, ret)
-            cv2.imshow('img', img)
-            cv2.waitKey(1000)
-ret_left, mtx_left, dist_left, rvecs_left, tvecs_left = cv2.calibrateCamera(objpoints_left, imgpoints_left, gray.shape[::-1], None, None)
+            #cv2.imshow('img', img)
+            #cv2.waitKey(1000)
+ret_left, mtx_left, dist_left, rvecs_left, tvecs_left = cv2.calibrateCamera(objpoints_left, imgpoints_left,gray.shape[::-1], None, None)
 img=cv2.imread('CalPicsLeft/CalLeft19.jpg')
 h,w=img.shape[:2]
-newcameramtx_left,roi_left=cv2.getOptimalNewCameraMatrix(mtx_left,dist_left,(w,h),0,(w,h))
+newcameramtx_left,roi_left=cv2.getOptimalNewCameraMatrix(mtx_left,dist_left,(w,h),1,(w,h))
 # save the matrices to a .yaml file
-specs={'newcameramtx_left':newcameramtx_left,'mtx_left':mtx_left,'dist_left':dist_left}
+specs={'newcameramtx_left':newcameramtx_left.tolist(),'mtx_left':mtx_left.tolist(),'dist_left':dist_left.tolist()}
 with open('calibrationDataLeft.yaml','w') as f:
     yaml.safe_dump(specs,f)
+
+with open('calibrationDataLeft.yaml') as f:
+    loaded=yaml.load(f,yaml.Loader)
+loaded=np.array(loaded)
+
+print(loaded)
+#print('   ')
+#print(specs)
+#print(specs['dist_left'])
+
+cv2.imshow('undistorted',cv2.undistort(img,mtx_left,dist_left,newcameramtx_left))
+cv2.waitKey(0)
