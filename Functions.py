f753743f19e0e29a206e0cd5a1533051796af635
Simon Kaserer <simon.kaserer@gmail.com>
2022-03-12 14:10:34 +0100

    Removed the warp and crop routine out from the contour extraction functions. Implemented a scaling for the contour image to show properly in the contour View

--- Functions.py
+++ Functions.py
@@ -65,14 +65,7 @@ def crop_image(warped_image):
     else:
         return None,None,None,None,None
 
-def extraction_polyDP(img,factor_epsilon,threshold_value,border_offset_px,every_nth_point,connectpoints,printsize,printpoints,show_outer_edge):
-    warped_image,framewidth,frameheight=warp_img(img,threshold_value,border_offset_px,show_outer_edge)
-    if warped_image is None:
-        return None,None
-    cropped_image,w,h,x,y=crop_image(warped_image)
-    if cropped_image is None:
-        return None,None            
-    # Find the rotated and cropped tool contour
+def extraction_polyDP(cropped_image,factor_epsilon:float,every_nth_point:int,connectpoints:bool,toolwidth:int,toolheight:int):
     cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     if len(cnts)>0:
         cnt=max(cnts,key=cv2.contourArea)
@@ -83,25 +76,26 @@ def extraction_polyDP(img,factor_epsilon,threshold_value,border_offset_px,every_
         tool_contour=cv2.approxPolyDP(cnt,epsilon,True)
         #inv=cv2.cvtColor(cropped_image,cv2.COLOR_GRAY2BGR)
         # create a black background
-        inv=np.zeros((int(h+10),int(w+10),3),dtype='uint8')
+        inv=np.zeros((int(toolheight+10),int(toolwidth+10),3),dtype='uint8')
         if connectpoints:
             img_cont=cv2.drawContours(inv,[tool_contour],-1,(0,255,0),2)
         else:
             img_cont=cv2.drawContours(inv,tool_contour,-1,(0,255,0),2) #only points
-        if printpoints:
-            print("Number of appr. points:")
-            print(len(tool_contour))
-        return tool_contour,img_cont
+        
+        # Scale the image to the width or height of the Contour View window
+        height,width,_=img_cont.shape
+        if width > height:
+            scale=500/width
+            
+        else:
+            scale=500/height
+        img_cont_scaled=cv2.resize(img_cont,(int(width*scale),int(height*scale)))
+        
+        return tool_contour,img_cont_scaled
     else:
         return None,None
 
-def extraction_TehChin(img,factor_epsilon,threshold_value,border_offset_px,every_nth_point,connectpoints,printsize,printpoints,show_outer_edge):
-    warped_image,framewidth,frameheight=warp_img(img,threshold_value,border_offset_px,show_outer_edge)
-    if warped_image is None:
-        return None,None
-    cropped_image,w,h,x,y=crop_image(warped_image)
-    if cropped_image is None:
-        return None,None  
+def extraction_TehChin(cropped_image,every_nth_point:int,connectpoints:bool,toolwidth:int,toolheight:int):
     # Find the rotated and cropped tool contour
     cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_TC89_L1)
     if len(cnts)>0:
@@ -109,26 +103,28 @@ def extraction_TehChin(img,factor_epsilon,threshold_value,border_offset_px,every
         cont=cnt.tolist()
         cont=cont[::every_nth_point]
         tool_contour=np.array(cont)
-        if printpoints:
-            print("Number of appr. points:")
-            print(len(tool_contour))
+       
         # create a black background
-        inv=np.zeros((int(h+10),int(w+10),3),dtype='uint8')
+        inv=np.zeros((int(toolheight+10),int(toolwidth+10),3),dtype='uint8')
         if connectpoints:
             img_cont=cv2.drawContours(inv,[tool_contour],-1,(0,255,0),2)
         else:
             img_cont=cv2.drawContours(inv,tool_contour,-1,(0,255,0),2) #only points
-        return tool_contour,img_cont
+        
+        # Scale the image to the width or height of the Contour View window
+        height,width,_=img_cont.shape
+        if width > height:
+            scale=500/width
+            
+        else:
+            scale=500/height
+        img_cont_scaled=cv2.resize(img_cont,(int(width*scale),int(height*scale)))
+        
+        return tool_contour,img_cont_scaled
     else:
         return None,None
 
-def extraction_convexHull(img,factor_epsilon,threshold_value,border_offset_px,every_nth_point,connectpoints,printsize,printpoints,show_outer_edge): 
-    warped_image,framewidth,frameheight=warp_img(img,threshold_value,border_offset_px,show_outer_edge)
-    if warped_image is None:
-        return None,None
-    cropped_image,w,h,x,y=crop_image(warped_image)
-    if cropped_image is None:
-        return None,None 
+def extraction_convexHull(cropped_image,every_nth_point:int,connectpoints:bool,toolwidth:int,toolheight:int): 
     # Look for the contour of the cropped and turned tool:
     cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     if len(cnts)>0:
@@ -144,17 +140,20 @@ def extraction_convexHull(img,factor_epsilon,threshold_value,border_offset_px,ev
             img_cont=cv2.drawContours(inv,[tool_hull],-1,(0,255,0),2)
         else:
             img_cont=cv2.drawContours(inv,tool_hull,-1,(0,255,0),2) #only points
-        return tool_hull,img_cont
+        # Scale the image to the width or height of the Contour View window
+        height,width,_=img_cont.shape
+        if width > height:
+            scale=500/width
+            
+        else:
+            scale=500/height
+        img_cont_scaled=cv2.resize(img_cont,(int(width*scale),int(height*scale)))
+        
+        return tool_hull,img_cont_scaled
     else:
         return None,None
 
-def extraction_None(img,factor_epsilon,threshold_value,border_offset_px,every_nth_point,connectpoints,printsize,printpoints,show_outer_edge):
-    warped_image,framewidth,frameheight=warp_img(img,threshold_value,border_offset_px,show_outer_edge)
-    if warped_image is None:
-        return None,None
-    cropped_image,w,h,x,y=crop_image(warped_image)
-    if cropped_image is None:
-        return None,None 
+def extraction_None(cropped_image,every_nth_point:int,connectpoints:bool,toolwidth:int,toolheight:int):
     # Find the rotated and cropped tool contour
     cnts,hierarchy=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     if len(cnts)>0:
@@ -163,20 +162,26 @@ def extraction_None(img,factor_epsilon,threshold_value,border_offset_px,every_nt
         cont=cont[::every_nth_point]
         tool_contour=np.array(cont)
         # create a black background
-        inv=np.zeros((int(h+10),int(w+10),3),dtype='uint8')
+        inv=np.zeros((int(toolheight+10),int(toolwidth+10),3),dtype='uint8')
         if connectpoints:
             img_cont=cv2.drawContours(inv,[tool_contour],-1,(0,255,0),2)
         else:
             img_cont=cv2.drawContours(inv,tool_contour,-1,(0,255,0),2) #only points
-        if printpoints:
-            print("Number of appr. points:")
-            print(len(tool_contour))
-        return tool_contour,img_cont
+        # Scale the image to the width or height of the Contour View window
+        height,width,_=img_cont.shape
+        if width > height:
+            scale=500/width
+            
+        else:
+            scale=500/height
+        img_cont_scaled=cv2.resize(img_cont,(int(width*scale),int(height*scale)))
+        
+        return tool_contour,img_cont_scaled
     else:
         return None,None
 
 
-def dxf_exporter(contour,path_and_name): 
+def dxf_exporter(contour,path_and_name,scaling): 
     file=dxf.new('R2000')
     msp=file.modelspace()
     points=[]
@@ -185,11 +190,11 @@ def dxf_exporter(contour,path_and_name):
     #add the first entry of the contour to the end for a closed contour in dxf
     cnt.append(cnt[0])
     for point in cnt:
-        points.append((point[0][0],point[0][1]))
+        points.append((point[0][0]/scaling,point[0][1]/scaling))
     msp.add_lwpolyline(points)
     file.saveas(path_and_name)
 
-def dxf_exporter_spline(contour,path_and_name): 
+def dxf_exporter_spline(contour,path_and_name,scaling): 
     # Spline is not working with InkScape - Fusion 360 works (Inventor should also)
     file=dxf.new('R2000')
     msp=file.modelspace()
@@ -199,7 +204,7 @@ def dxf_exporter_spline(contour,path_and_name):
     #add the first entry of the contour to the end for a closed contour in dxf
     cnt.append(cnt[0])
     for point in cnt:
-        points.append((point[0][0],point[0][1],0))
+        points.append((point[0][0]/scaling,point[0][1]/scaling,0))
     msp.add_spline(points)
     file.saveas(path_and_name)
 
