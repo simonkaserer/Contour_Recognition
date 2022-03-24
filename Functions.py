5b28bdc9cc00433d7d605df4f7fa4026d88ac9a7
Simon Kaserer <simon.kaserer@gmail.com>
2022-03-24 09:03:15 +0100

    Deleted the spline export function and the image turning function

--- Functions.py
+++ Functions.py
@@ -46,12 +46,12 @@ def warp_img(img,threshold_value:int,border_offset_px:int,show_outer_edge:bool):
                 # crop the image to remove the outer edge (offset can maybe be smaller when camera calibration is done?)
             warped_image=warped_image[0+border_offset_px:width-border_offset_px,0+border_offset_px:height-border_offset_px]
             
-            return warped_image,width,height
+            return warped_image,width,height,input_pts
             
         else:   
-            return None,None,None
+            return None,None,None,None
     else:
-        return None,None,None
+        return None,None,None,None
 
 def crop_image(warped_image):
     # Look for the contour of the tool:
@@ -230,6 +230,53 @@ def extraction_spline(cropped_image,every_nth_point:int,connectpoints:bool,toolw
    else:
         return None,None
 
+def extraction_spline_tehChin(cropped_image,every_nth_point:int,connectpoints:bool,toolwidth:int,toolheight:int):
+   
+   cnts,_=cv2.findContours(cropped_image,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_TC89_L1)
+   if len(cnts)>0:
+      cnt=max(cnts,key=cv2.contourArea)
+      cont=cnt.tolist()
+      cont=cont[::every_nth_point]
+      number_of_points=len(cont)
+      #cont.append(cont[0])
+      tool_contour=np.array(cont)
+      #Rearrange the points
+      points=np.array([[tool_contour[i][0][0],tool_contour[i][0][1]] for i in range(number_of_points)])
+
+      # Add the last point to the list by padding 
+      #pad=3
+      #points=np.pad(points,[(pad,pad),(0,0)],mode='wrap')
+      x,y=points.T
+      
+      i=np.arange(0,len(points))
+
+      interp_i=np.linspace(0,i.max(),5*i.max())
+
+      x_new=interp1d(i,x,kind='cubic')(interp_i)
+      y_new=interp1d(i,y,kind='cubic')(interp_i)
+
+      cnt=np.array([[[int(x_new[i]),int(y_new[i])]for i in range(len(x_new))]])
+
+      # create a black background
+      inv=np.zeros((int(toolheight+2),int(toolwidth+2),3),dtype='uint8')
+      if connectpoints:
+         img_cont=cv2.drawContours(inv,[cnt],-1,(0,255,0),1)
+      else:
+         img_cont=cv2.drawContours(inv,cnt,-1,(0,255,0),1) #only points
+      
+      # Scale the image to the width or height of the Contour View window
+      height,width,_=img_cont.shape
+      if width > height:
+            scale=500/width
+            
+      else:
+            scale=500/height
+      img_cont_scaled=cv2.resize(img_cont,(int(width*scale),int(height*scale)))
+      
+      return tool_contour,img_cont_scaled 
+   else:
+        return None,None
+
 def dxf_exporter(contour,path_and_name,scaling_width,scaling_height): 
     file=dxf.new('R2000')
     msp=file.modelspace()
@@ -243,23 +290,9 @@ def dxf_exporter(contour,path_and_name,scaling_width,scaling_height):
     msp.add_lwpolyline(points)
     file.saveas(path_and_name)
 
-def dxf_exporter_spline(contour,path_and_name,scaling): 
-    # Spline is not working with InkScape - Fusion 360 works (Inventor should also)
-    file=dxf.new('R2000')
-    msp=file.modelspace()
-    points=[]
-    #convert the contour to a list:
-    cnt=contour.tolist()
-    #add the first entry of the contour to the end for a closed contour in dxf
-    cnt.append(cnt[0])
-    for point in cnt:
-        points.append((point[0][0]/scaling,point[0][1]/scaling,0))
-    msp.add_spline(points)
-    file.saveas(path_and_name)
-
 def toolheight(img_left,img_right):
-    warped_left,framew_left,frameh_left=warp_img(img_left,150,1,False)
-    warped_right,framew_right,fdrameh_left=warp_img(img_right,150,1,False)
+    warped_left,framew_left,frameh_left,pts_left=warp_img(img_left,150,1,False)
+    warped_right,framew_right,fdrameh_left,pts_right=warp_img(img_right,150,1,False)
     
     cropped_left,toolw_left,toolh_left,x_left,y_left=crop_image(warped_left)
     cropped_right,toolw_right,toolh_right,x_right,y_right=crop_image(warped_right)
@@ -267,53 +300,4 @@ def toolheight(img_left,img_right):
     cv2.imshow('height',stack)
     print(f'dx:{x_right-x_left}, dy:{y_right-y_left}')
     print(f'squared sum:{(x_right-x_left)**2+(y_right-y_left)**2}')
-
-def rotate_img(img,x_angle,y_angle,z_angle):
-   #From yt: v=bbSFk4vTXSI
-   if x_angle<0:
-      x_angle=360+x_angle
-   if y_angle<0:
-      y_angle=360+y_angle
-   if z_angle<0:
-      z_angle=360+z_angle
-   w=img.shape[1]
-   h=img.shape[0]
-   f=(np.sqrt(h**2+w**2))/(2*np.sin(z_angle*np.pi/np.float32(180)) if np.sin(z_angle*np.pi/np.float32(180)) != 0 else 0.000001)
-   
-   RX=np.array([[1,0,0,0],
-                [0,np.cos((x_angle*np.pi/np.float32(180))),-np.sin((x_angle*np.pi/np.float32(180))),0],
-                [0,np.sin((x_angle*np.pi/np.float32(180))),np.cos((x_angle*np.pi/np.float32(180))),0],
-                [0,0,0,1]])
-   RY=np.array([[np.cos((y_angle*np.pi/np.float32(180))),0,-np.sin((y_angle*np.pi/np.float32(180))),0],
-                [0,1,0,0],
-                [np.sin((y_angle*np.pi/np.float32(180))),0,np.cos((y_angle*np.pi/np.float32(180))),0],
-                [0,0,0,1]])
-   RZ=np.array([[np.cos((z_angle*np.pi/np.float32(180))),-np.sin((z_angle*np.pi/np.float32(180))),0,0],
-                [np.sin((z_angle*np.pi/np.float32(180))),np.cos((z_angle*np.pi/np.float32(180))),0,0],
-                [0,0,1,0],
-                [0,0,0,1]]) 
-                                  
-   # Calculate the rotation matrix
-   R=np.dot(np.dot(RX,RY),RZ)
-
-   # Translation matrix
-   T=np.array([[1,0,0,max(w,h)/3],
-               [0,1,0,-max(w,h)/2],
-               [0,0,1,f],
-               [0,0,0,1]])
-
-   # Projection of 2D matrix -> 3D matrix
-   A1=np.array([[1,0,-w/2],
-                [0,1,-h/2],
-                [0,0,1],
-                [0,0,1]])   
-
-   # Projection of 3D matrix -> 2D matrix                
-   A2=np.array([[f,0,w,0],
-                [0,f,h,0],
-                [0,0,1,0]])                            
-   M=np.dot(A2,np.dot(T,np.dot(R,A1)))
-
-   img_rot=cv2.warpPerspective(img,M,(max(w,h),max(w,h)),flags=cv2.INTER_NEAREST)
-
-   return img_rot   
+    print(f'pts left: {pts_left}\npts right: {pts_right}')
