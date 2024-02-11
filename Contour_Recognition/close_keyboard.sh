1cb70d705619607645f4a8bc44972a9a685b7c42
Simon <simon.kaserer@gmail.com>
2024-02-11 13:53:49 +0100

    Deleted the bash scripts for the old keyboard

--- Contour_Recognition/close_keyboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# check if the keyboard is already running, then start it if not 
-if pgrep  onboard > /dev/null
-then
-# Get the pid and stop it 
-pid=$(grep -E '[0-9]' ./keyboard.pid)
-kill -15 $pid 
-fi
